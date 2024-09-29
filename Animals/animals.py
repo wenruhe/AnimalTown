e225b9862f155dcdedf6eead1d5ec910c2fee798
Wenru He <he525@purdue.edu>
2024-09-29 10:50:08 -0400

    init

---
+++ Animals/animals.py
@@ -0,0 +1,15 @@
+import pydantic
+import uuid
+from typing import Literal
+from memory import Memory
+
+class Animal(pydantic.BaseModel):
+    _id: uuid.UUID = pydantic.Field(default_factory=uuid.uuid4)
+    name: str
+    age: int
+    gender: Literal['Female', 'Male']
+    job: str
+    personality: str
+    daily_routine: str
+    memory: pydantic.Field(Memory)
+
