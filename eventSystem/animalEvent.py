67ef70d3efaecdb5cc66a584ffb45575f245d07a
SonggPro <95326810+SonggPro@users.noreply.github.com>
2024-10-20 00:34:42 +0800

    修正了一些小问题

--- eventSystem/animalEvent.py
+++ eventSystem/animalEvent.py
@@ -1,9 +1,10 @@
 import time
 
-class Event:
+class AnimalEvent:
     def __init__(self, trigger_condition, event_name, involved_animals, suggested_by, environment_effects=None):
         self.trigger_condition = trigger_condition  # 事件触发条件
         self.event_name = event_name  # 事件名称
+        self.event_content = None  # 事件内容
         self.involved_animals = involved_animals  # 涉及的动物
         self.suggested_by = suggested_by  # 提议者
         self.start_time = None  # 事件开始时间
@@ -30,14 +31,15 @@ class Event:
             self.involved_animals.append(animal)
             print(f"{animal} has joined the event '{self.event_name}'.")
     # 更新动物之间的关系变化
-    def update_relationships(self, animal_1, animal_2, change: dict):
-        relationships = Relationships()
+    def update_relationships(self, animal_1, animal_2, change: dict, relationships):
         relationships.update_relationship_bidirectional(animal_1, animal_2, change)
         # if animal_1 in self.involved_animals:
         #     self.involved_animals[animal_1]["relationship_changes"][animal_2] = change
         # if animal_2 in self.involved_animals:
         #     self.involved_animals[animal_2]["relationship_changes"][animal_1] = change
         self.event_logs.append(f"Relationship between {animal_1} and {animal_2} changed by {change}.")
+    def exe_event(self):
+        pass
 
 
 
