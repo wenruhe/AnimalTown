92acbc25739216f0c82115c9ffc61a43a1975a0f
Wenru He <he525@purdue.edu>
2024-10-19 20:30:03 -0400

    change back

--- Animals/animals.py
+++ Animals/animals.py
@@ -1,44 +1,17 @@
-import time
+from pydantic import BaseModel, Field
+import uuid
+from typing import Literal, List
+from db.userConfigs import user_credentials
 
-class AnimalEvent:
-    def __init__(self, trigger_condition, event_name, involved_animals, suggested_by, environment_effects=None):
-        self.trigger_condition = trigger_condition  # 事件触发条件
-        self.event_name = event_name  # 事件名称
-        self.event_content = None  # 事件内容
-        self.involved_animals = involved_animals  # 涉及的动物
-        self.suggested_by = suggested_by  # 提议者
-        self.start_time = None  # 事件开始时间
-        self.end_time = None  # 事件结束时间
-        self.duration = None  # 事件持续时间
-        self.event_status = "pending"  # 事件状态: "pending", "in_progress", "completed"
-        self.event_outcome = None  # 事件结果
-        self.memory_record = []  # 用于记录事件对记忆系统的影响
-        self.environment_effects = environment_effects if environment_effects else {}  # 环境变化
-        self.relationship_changes = {}  # 动物之间关系的变化
+class Animal(BaseModel):
+    userID: str = user_credentials.USERID # 和用户账户相关联
+    animalID: str = Field(default_factory=lambda: str(uuid.uuid4())) # 每个用户下的每个动物拥有一个独立ID
+    name: str
+    age: int
+    gender: Literal['Female', 'Male']
+    job: str
+    personality: List[str]
+    memory: List[str] = []
 
-    
-    def trigger(self):
-        if self.trigger_condition():
-            self.start_time = time.time()
-            self.event_status = "in_progress"
-            print(f"Event '{self.event_name}' started!")
-        else:
-            print(f"Trigger condition for '{self.event_name}' not met.")
-            
-    def add_involved_animal(self, animal):
-        """增加参与事件的动物"""
-        if animal not in self.involved_animals:
-            self.involved_animals.append(animal)
-            print(f"{animal} has joined the event '{self.event_name}'.")
-    # 更新动物之间的关系变化
-    def update_relationships(self, animal_1, animal_2, change: dict, relationships):
-        relationships.update_relationship_bidirectional(animal_1, animal_2, change)
-        # if animal_1 in self.involved_animals:
-        #     self.involved_animals[animal_1]["relationship_changes"][animal_2] = change
-        # if animal_2 in self.involved_animals:
-        #     self.involved_animals[animal_2]["relationship_changes"][animal_1] = change
-        self.event_logs.append(f"Relationship between {animal_1} and {animal_2} changed by {change}.")
-    def exe_event(self):
-        pass
 
 
